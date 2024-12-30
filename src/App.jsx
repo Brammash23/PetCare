c52a1d6e0ba24497364347432957b697e9e848e7
Brammash23 <brammash670@gmail.com>
2024-12-30 16:59:54 +0530

    Remove Unwanted Components

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ import { Blogs } from "./Blogs";
 import { ContactUs } from "./ContactUs";
 import { AdoptCatlog } from "./AdoptCatlog";
 import { About } from "./About";
-import { Account } from "./Account/Account";
 import { MyAccount } from "./myaccount";
-import { Adopt } from "./Common/Adopt";
-import { PetInfo } from "./PetInfo";
+
 import { AboutPet } from "./aboutpet";
-import { Adaption } from "./Adaption";
+
 import { Adaption_Verify } from "./adaption_verify";
 import { ViewAdoptions } from "./ViewAdoptions";
 import { GetInvolved } from "./GetInvolved";
